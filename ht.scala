00842be45b607337e7f9b57f675b7b9bba135dda
rheber <rheber@rediffmail.com>
2016-04-11 22:43:16 +1000

    beginnings of parser

--- ht.scala
+++ ht.scala
@@ -1,8 +1,24 @@
 import scala.io.StdIn
+import scala.util.parsing.combinator._
 
-var input = " "
-while(input != "") {
-  print("ht> ")
-  input = StdIn.readLine()
-  println(input)
+class Command extends JavaTokenParsers {
+  def cmd: Parser[String] = quit
+  def quit: Parser[String] = "quit"
 }
+
+object ParseCommand extends Command {
+  def run() {
+    var input = " "
+    while(input != "") {
+      print("ht> ")
+      input = StdIn.readLine()
+      var p = parseAll(cmd, input)
+      p match {
+        case Success("quit", _) => return
+        case _ => println("Unrecognised command")
+      }
+    }
+  }
+}
+
+ParseCommand.run()
