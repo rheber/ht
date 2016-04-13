414b52c40f90864701cf32de5ebdc235d3524801
rheber <rheber@rediffmail.com>
2016-04-13 23:54:03 +1000

    make and list tasks

--- ht.scala
+++ ht.scala
@@ -2,19 +2,25 @@ import scala.io.StdIn
 import scala.util.parsing.combinator._
 
 class Command extends JavaTokenParsers {
-  def cmd: Parser[String] = quit
+  def cmd: Parser[String] = list|quit|weekly
+  def list: Parser[String] = "list weekly"
   def quit: Parser[String] = "quit"
+  def weekly: Parser[String] = "weekly "~>"""[^\n]+""".r
 }
 
 object ParseCommand extends Command {
   def run() {
+    var weekly = List[String]()
+
     var input = " "
     while(input != "") {
       print("ht> ")
       input = StdIn.readLine()
       var p = parseAll(cmd, input)
       p match {
+        case Success("list weekly", _) => weekly.foreach(println)
         case Success("quit", _) => return
+        case Success(task, _) => weekly = task::weekly
         case _ => println("Unrecognised command")
       }
     }
