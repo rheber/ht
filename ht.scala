6fdce582606bb2c90c8a6f7f4ef607a7536f87da
rheber <rheber@rediffmail.com>
2016-04-15 20:17:53 +1000

    delete tasks

--- ht.scala
+++ ht.scala
@@ -1,26 +1,37 @@
+import scala.collection.mutable.Map
 import scala.io.StdIn
 import scala.util.parsing.combinator._
 
 class Command extends JavaTokenParsers {
-  def cmd: Parser[String] = list|quit|weekly
+  def cmd: Parser[Any] = delete|list|quit|weekly
+  def number: Parser[String] = """\d+""".r
+
+  def delete: Parser[Int] = "delete"~>number^^(_.toInt)
   def list: Parser[String] = "list weekly"
   def quit: Parser[String] = "quit"
   def weekly: Parser[String] = "weekly "~>"""[^\n]+""".r
 }
 
 object ParseCommand extends Command {
-  def run() {
-    var weekly = List[String]()
+  val tasks = Map[Int,String]()
+  var nextInt = 1
 
+  def addTask(task:String) {
+    tasks += (nextInt->task)
+    nextInt += 1
+  }
+
+  def run() {
     var input = " "
     while(input != "") {
       print("ht> ")
       input = StdIn.readLine()
       var p = parseAll(cmd, input)
       p match {
-        case Success("list weekly", _) => weekly.foreach(println)
+        case Success("list weekly", _) => tasks.foreach(println)
         case Success("quit", _) => return
-        case Success(task, _) => weekly = task::weekly
+        case Success(num:Int, _) => tasks -= num
+        case Success(task:String, _) => addTask(task)
         case _ => println("Unrecognised command")
       }
     }
