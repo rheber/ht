0e15afa8c04875900533a705589754a8e20ff1a6
rheber <rheber@rediffmail.com>
2016-04-19 23:07:18 +1000

    renumber

--- ht.scala
+++ ht.scala
@@ -6,16 +6,18 @@ case class CmdDelete(num: Int)
 case class CmdList(name: String)
 case class CmdMonthly(name: String)
 case class CmdQuit()
+case class CmdRenumber()
 case class CmdWeekly(name: String)
 
 class Command extends JavaTokenParsers {
-  def cmd: Parser[Any] = delete|list|monthly|quit|weekly
+  def cmd: Parser[Any] = delete|list|monthly|quit|renumber|weekly
   def number: Parser[Int] = """\d+""".r^^(_.toInt)
 
   def delete: Parser[CmdDelete] = "delete"~>number^^CmdDelete
   def list: Parser[CmdList] = "list "~>"""[^\n]+""".r^^CmdList
   def monthly: Parser[CmdMonthly] = "monthly "~>"""[^\n]+""".r^^CmdMonthly
   def quit: Parser[CmdQuit] = "quit"^^(_=>CmdQuit())
+  def renumber: Parser[CmdRenumber] = "renumber"^^(_=>CmdRenumber())
   def weekly: Parser[CmdWeekly] = "weekly "~>"""[^\n]+""".r^^CmdWeekly
 }
 
@@ -24,16 +26,23 @@ class Task(val name:String, val interval:String) {
 }
 
 object ParseCommand extends Command {
-  val tasks = Map[Int,Task]()
+  var tasks = Map[Int,Task]()
   var nextInt = 1
 
-  def addTask(name:String, interval:String) {
+  def addTask(name:String, interval:String, tasks:Map[Int, Task]) {
     tasks += (nextInt->new Task(name, interval))
     nextInt += 1
   }
 
   def printlnTask(task:Tuple2[Int, Task]) {
     printf("%3d [ ] %s\n", task._1, task._2)
+  }
+
+  def renumberTasks() {
+    val newTasks = Map[Int, Task]()
+    nextInt = 1
+    tasks.foreach(x=>addTask(x._2.name, x._2.interval, newTasks))
+    tasks = newTasks
   }
 
   def run() {
@@ -47,9 +56,10 @@ object ParseCommand extends Command {
         case Success(CmdList("all"), _) => tasks.foreach(printlnTask)
         case Success(CmdList(itvl), _) =>
             tasks.filter(_._2.interval == itvl).foreach(printlnTask)
-        case Success(CmdMonthly(task), _) => addTask(task, "monthly")
+        case Success(CmdMonthly(task), _) => addTask(task, "monthly", tasks)
         case Success(CmdQuit(), _) => return
-        case Success(CmdWeekly(task), _) => addTask(task, "weekly")
+        case Success(CmdRenumber(), _) => renumberTasks()
+        case Success(CmdWeekly(task), _) => addTask(task, "weekly", tasks)
         case _ => println("Unrecognised command")
       }
     }
