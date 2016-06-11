a22a73f44fc1d6f8e5080c7bbf5f38fa9c0f8005
rheber <rheber@rediffmail.com>
2016-06-11 15:58:13 +1000

    quit!

--- ht.scala
+++ ht.scala
@@ -8,6 +8,7 @@ case class CmdDelete(num: Int)
 case class CmdList(name: String)
 case class CmdMonthly(name: String)
 case class CmdQuit()
+case class CmdSafeQuit()
 case class CmdRenumber()
 case class CmdSave()
 case class CmdWeekly(name: String)
@@ -17,6 +18,7 @@ class Command extends JavaTokenParsers {
                          list|
                          monthly|
                          quit|
+                         safequit|
                          renumber|
                          save|
                          weekly
@@ -25,7 +27,8 @@ class Command extends JavaTokenParsers {
   def delete: Parser[CmdDelete] = "delete"~>number^^CmdDelete
   def list: Parser[CmdList] = "list "~>"""[^\n]+""".r^^CmdList
   def monthly: Parser[CmdMonthly] = "monthly "~>"""[^\n]+""".r^^CmdMonthly
-  def quit: Parser[CmdQuit] = "quit"^^(_=>CmdQuit())
+  def quit: Parser[CmdQuit] = "quit!"^^(_=>CmdQuit())
+  def safequit: Parser[CmdSafeQuit] = "quit"^^(_=>CmdSafeQuit())
   def renumber: Parser[CmdRenumber] = "renumber"^^(_=>CmdRenumber())
   def save: Parser[CmdSave] = "save"^^(_=>CmdSave())
   def weekly: Parser[CmdWeekly] = "weekly "~>"""[^\n]+""".r^^CmdWeekly
@@ -37,11 +40,18 @@ class Task(val name:String, val interval:String) {
 
 object ParseCommand extends Command {
   var tasks = Map[Int,Task]()
+  var modified = false
   var nextInt = 1
 
   def addTask(name:String, interval:String, tasks:Map[Int, Task]) {
     tasks += (nextInt->new Task(name, interval))
     nextInt += 1
+    modified = true
+  }
+
+  def deleteTask(num:Int) {
+    tasks -= num
+    modified = true
   }
 
   def printlnTask(task:Tuple2[Int, Task]) {
@@ -71,26 +81,34 @@ object ParseCommand extends Command {
     }
   }
 
+  def safeQuit() {
+    if(!modified) { System.exit(0) }
+    println("Unsaved changes, either 'save' or 'quit!'")
+  }
+
   def saveTasks() {
     val f = new FileWriter("./habits")
     tasks.foreach(x=>f.write("%s\t%s\n".format(x._2.name, x._2.interval)))
     f.close()
+    modified = false
   }
 
   def run() {
     var input = " "
     loadTasks()
+    modified = false // because loadTasks calls addTasks
     while(input != "") {
       print("ht> ")
       input = StdIn.readLine()
       var p = parseAll(cmd, input)
       p match {
-        case Success(CmdDelete(num), _) => tasks -= num
+        case Success(CmdDelete(num), _) => deleteTask(num)
         case Success(CmdList("all"), _) => tasks.foreach(printlnTask)
         case Success(CmdList(itvl), _) =>
             tasks.filter(_._2.interval == itvl).foreach(printlnTask)
         case Success(CmdMonthly(task), _) => addTask(task, "monthly", tasks)
         case Success(CmdQuit(), _) => return
+        case Success(CmdSafeQuit(), _) => safeQuit()
         case Success(CmdRenumber(), _) => renumberTasks()
         case Success(CmdSave(), _) => saveTasks()
         case Success(CmdWeekly(task), _) => addTask(task, "weekly", tasks)
